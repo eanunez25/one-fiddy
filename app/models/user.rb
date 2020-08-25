000a5f07271e02bc0de38d625bc0203aea10ae44
Elvin Nunez <eanunez25@gmail.com>
2020-08-24 19:34:10 -0500

    add password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   USERNAME_REGEX = /\A^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$\z/i
   validates :username, presence: true, uniqueness: true, length: { minimum: 5, maximum: 12 }, format: { with: USERNAME_REGEX }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_061539) do
+ActiveRecord::Schema.define(version: 2020_08_25_003028) do
 
   create_table "results", force: :cascade do |t|
     t.integer "wpm"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_061539) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "results", "users"
