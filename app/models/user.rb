3d190a31f2926b73dabedb85f766433ee071cb0d
Elvin Nunez <eanunez25@gmail.com>
2020-08-24 19:23:04 -0500

    update signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  USERNAME_REGEX = /\A^[a-zA-Z0-9](_(?!(\.|_))|\.(?!(_|\.))|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$\z/i
+  validates :username, presence: true, uniqueness: true, length: { minimum: 5, maximum: 12 }, format: { with: USERNAME_REGEX }
 end
