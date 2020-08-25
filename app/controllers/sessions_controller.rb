166263f79e8bc9f47b0eb4f6ce0ea0e701ded49a
Elvin Nunez <eanunez25@gmail.com>
2020-08-24 21:58:13 -0500

    mobile friendly?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else  
+      flash.now[:danger] = "Invalid username/password comination."
+      render 'new'
+    end
   end
 
   def destroy
