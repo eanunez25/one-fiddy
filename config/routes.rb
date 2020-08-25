2bfa1390b1ebc5f6c039d102040c2f35eba49cb6
Elvin Nunez <eanunez25@gmail.com>
2020-08-24 20:58:48 -0500

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'about', to: 'static_pages#about'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :results
   resources :users
+  resources :sessions
 
   root 'static_pages#home'
 end
